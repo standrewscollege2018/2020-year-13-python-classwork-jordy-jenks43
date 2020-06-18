dda9ba321b5920fb6246088970f73ada02901748
jordy-jenks43 <jje2174@hotmail.com>
2020-06-19 11:40:59 +1200

    Update student management.py
    fixed students class finder function

--- student management.py
+++ student management.py
@@ -1,5 +1,43 @@
 '''this program is able to manage a student data base'''
 
+class Student:
+    '''the student class stores student's name, age phone number, if they are enrolled, gender, and a list of their classes'''
+    
+    '''setting up all attributes for students within the list from the student csv file'''
+    def __init__(self, name, age, phone, gender, classes):
+        self._name = name
+        self._age = age
+        self._phone = phone
+        self._gender = gender
+        self._classes = classes
+        self.enrolled = True
+        student_list.append(self)
+    
+    '''getter function for getting student names'''
+    def get_name(self):
+        return self._name
+    
+    '''getter function for student age'''
+    def get_age(self):
+        return self._age
+    
+    '''getter function for phone numbers'''
+    def get_phone(self):
+        return self._phone
+    
+    '''getter function for student enrollment status'''
+    def get_enrolled(self):
+        return self._enrolled
+    
+    '''getter function for student genders'''
+    def get_gender(self):
+        return self._gender
+    
+    '''getter function for student enrolled class'''
+    def get_classes(self):
+        return self._classes
+
+'''generating the students via custom function'''
 def generateStudents():
     import csv
     with open ('myRandomStudents.csv', newline='') as csvfile:
@@ -12,48 +50,17 @@ def generateStudents():
                 i += 1
             Student(line[0], int(line[1]), line[2], line[3], classes)
 
-class Student:
-    '''the student class stores student's name, age phone number, if they are enrolled, gender, and a list of their classes'''
+'''function for finding the number of students enrolled to a subject'''
+def find_class():
+    student_class_selection = input("Input a class: ")
+    count = 0    
+    for student in student_list:
+        if student_class_selection.upper() in student.get_classes():
+            count += 1
     
-    def __init__(self, name, age, phone, gender, classes):
-        self._name = name
-        self._age = age
-        self._phone = phone
-        self._gender = gender
-        self._classes = classes
-        student_list.append(self)
+    print(count)    
     
-    def get_name(self):
-        return self._name
-    
-    def get_age(self):
-        return self._age
-    
-    def get_phone(self):
-        return self._phone
-    
-    def get_enrolled(self):
-        return self._enrolled
-    
-    def get_gender(self):
-        return self._gender
-    
-    def get_classes(self):
-        return self._classes
-
 student_list = []
 
 generateStudents()
-
-print(student.get_classes())
-
-'''
-student_class_selection = input("Input a class: ")
-count = 0
-
-for student in student_list:
-    if student_class_selection.lower() in student.get_classes():
-        count += 1
-
-print(count)
-'''
+find_class()
