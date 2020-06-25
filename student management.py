2dd9b2632244ab50e1da1071ef4c06de351e5dab
jordy-jenks43 <jje2174@hotmail.com>
2020-06-25 13:03:04 +1200

    Update student management.py
    Added error catching.

--- student management.py
+++ student management.py
@@ -58,29 +58,60 @@ def find_class():
         if student_class_selection.upper() in student.get_classes():
             count += 1
     if count == 0:
-        print("There are no students in this class!")
+        input("There are no studentes in", student_class_selection.upper())
     else:
-        print(count)    
+        print("{} in {}".format(count, student_class_selection.upper()))
+        input("Press enter to continue")
+
 
 '''function for printing the name of students that signed up for the class'''
 def students_in_class():
+    count = 0
     student_class_selection = input("Which class are you looking for?: ")
     for student in student_list:
         if student_class_selection.upper() in student.get_classes():
-            print(student.get_name())
+            count+= 1
+            print("{}. {}".format(i, student.get_name()))
+    if count == 0:
+        input("No students found!")
+        
 
 '''function for printing out specific student information'''
 def find_student():
+    count = 0
+    find_student = []
     student_search_input = input("Who are you looking for? ")
     for student in student_list:
         if student_search_input.title() == student.get_name():
-            print(student.get_name())
-            print(student.get_age())
-            print(student.get_phone())
-            print(student.get_gender())
-            print(student.get_classes())
-
+            find_student = [student.get_name(), student.get_age(), student.get_phone(), student.get_gender(), student.get_classes()]
+            count += 1
+    if count == 0:
+        input("No students found!")
+    else:
+        for i in range(len(find_student)):
+            print("-", find_student[i])
+                
 student_list = []
 
 generateStudents()
-find_student()
+
+tf = True
+while tf == True:
+    print("1. Find the number of students in a class")
+    print("2. Find students in a class")
+    print("3. Find a specific student")
+    print("4. Quit")
+    try:
+        user_choice = int(input("What would you like to do? "))
+        if user_choice == 1:
+            find_class()
+        elif user_choice == 2:
+            students_in_class()
+        elif user_choice == 3:
+            find_student()
+        else:
+            sure = input("Are you sure? y/n: ")
+            if sure.lower() == 'y':
+                tf = False
+    except:
+        print("Use only numbers to operate the menu!")
