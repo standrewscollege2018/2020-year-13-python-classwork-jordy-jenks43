9f1c9866f9bfa3d690f6d16a153f0857051f6eaf
jordy-jenks43 <jje2174@hotmail.com>
2020-07-29 09:58:19 +1200

    Delete TextFile1.py

--- TextFile1.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('protagonist.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["SN", "Movie", "Protagonist"])
-    writer.writerow([1, "Lord of the Rings", "Frodo Baggins"])
-    writer.writerow([2, "Harry Potter", "Harry Potter"])
